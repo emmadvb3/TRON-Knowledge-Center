625be00e48a2bc4acf44708298f1e630d5a34258
emmadvb3 <158344518+emmadvb3@users.noreply.github.com>
2024-12-17 11:25:18 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+
+//Links to the buttons in the top "what is TRON"section
+
+function overviewLink(){
+  window.open ("https://confluence.disney.com/display/TUC/Overview+and+TRON+Fun+Facts","_blank");
+  }
+
+function releaseNotesLink(){
+  window.open ("https://confluence.disney.com/pages/viewpage.action?pageId=917679206","_blank");
+  }
+  
+function requestAccessLink(){
+  window.open ("https://confluence.disney.com/display/TUC/Request+TRON+Access","_blank");
+  }
+      
+function userGuidesLink(){
+  window.open ("https://confluence.disney.com/pages/viewpage.action?spaceKey=TUC&title=User+Guides","_blank");
+  }
+          
+//Links to the cards in the bottom "TRON Academy" Section...Need to determine what these links should be. right now i left them blank. 
+
+
+function tron101Link(){
+    window.open ("","_blank");
+    }
+
+function developmentLink(){
+    window.open ("","_blank");
+    }
+
+function tronAdminLink(){
+    window.open ("","_blank");
+    }
